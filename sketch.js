b0190e2d596e5a6a1e45aabf222a147d4ac277fc
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-08-22 18:44:17 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var car,wall;
+var speed, weight
+function setup() {
+  createCanvas(800,400);
+  speed=random(55,90)
+  weight=random(400,1500)
+  createSprite(400, 200, 50, 50);
+  car=createSprite(50,200,50,50);
+  wall=createSprite(150,200,60,height/2)
+  car.velocityX = speed;
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
