b85cbc5bd13ad45ccda83c1588cbc8e3ce66d040
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2020-10-11 17:08:48 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,20 +8,20 @@ car = createSprite(50,200,50,50);
 wall = createSprite(1500,200,50,50);
   car.velocityX = speed;
   if(speed< 100){
-    car.shapeColor(0,255,0);
+    car.shapeColor=color(0,255,0);
   } 
 
   if(speed = (100-180)){
-    car.shapeColor(230,230,0);
+    car.shapeColor=color(230,230,0);
   } 
 
   if(speed > 180){
-    car.shapeColor(255,0,0);
+    car.shapeColor=color(255,0,0);
   } 
 }
 
 function draw() {
-  background(255,255,255); 
+  background(0,0,0); 
   car.collide(wall);
   drawSprites();
 }
